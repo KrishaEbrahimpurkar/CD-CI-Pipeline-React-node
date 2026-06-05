f57717ca8086116973642413f8be3963b5c10733
Krisha Ebrahimpurkar <kebrahimpurkar@gmail.com>
2026-06-05 18:18:01 +0530

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import "./App.css";
+
+function App() {
+
+  return (
+    <div className="container">
+
+      <h1>
+        React Node CI/CD Pipeline
+      </h1>
+
+      <p>
+        Automated Build, Test and Deployment
+      </p>
+
+      <div className="status">
+        Application Running Successfully
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ server/tests/app.test.js
@@ -0,0 +1,17 @@
+const request = require("supertest");
+const app = require("../app");
+
+describe("API Testing", () => {
+
+    test("GET /health", async () => {
+
+        const response = await request(app)
+            .get("/health");
+
+        expect(response.statusCode).toBe(200);
+
+        expect(response.body.status)
+            .toBe("UP");
+    });
+
+});
